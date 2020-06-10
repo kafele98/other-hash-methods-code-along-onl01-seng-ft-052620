af3bca467a5ae0b305dff25de89883d9d8866616
Zoe Keller <keller.zoe98@gmail.com>
2020-06-10 01:17:15 +0000

    Automatically backed up by Learn

--- lib/min.rb
+++ lib/min.rb
@@ -9,10 +9,6 @@ require 'pry'
 # }
 
 def get_the_min(groceries)
-  letters= {a: 1, b:2}
-  
-  letters.min 
-  
-  alphabetical_order = {c: 100, d:1}
-  alphabetical_order.min 
+  groceries.min 
+
 end
